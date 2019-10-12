8cd5f85022e99f3e2a152b332aeda79479c6aa4d
darthleonard <leolinuxmx@gmail.com>
2019-10-12 17:46:35 -0500

    heroe component

--- 02-spa/src/app/servicios/heroes.service.ts
+++ 02-spa/src/app/servicios/heroes.service.ts
@@ -62,6 +62,10 @@ export class HeroesService {
     getHeroes(): Heroe[] {
         return this.heroes;
     }
+
+    getHeroe(idx: number): Heroe {
+      return this.heroes[idx];
+  }
 }
 
 export interface Heroe {
