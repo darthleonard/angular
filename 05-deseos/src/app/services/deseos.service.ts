3b1a6a6a30be904d14ff16af944808eb0416189e
darthleonard <leolinuxmx@gmail.com>
2019-10-13 19:43:29 -0500

    servicio y clases de lista deseos

---
+++ 05-deseos/src/app/services/deseos.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeseosService {
+  listas: Lista[] = [];
+
+  constructor() { }
+}
