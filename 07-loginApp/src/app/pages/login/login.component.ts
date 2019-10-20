ed4dce3be2afc589306f8ce94b0e0dd2389412bf
darthleonard <leolinuxmx@gmail.com>
2019-10-20 17:27:26 -0500

    pantalla login y validaciones

--- 07-loginApp/src/app/pages/login/login.component.ts
+++ 07-loginApp/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  usuario: UsuarioModel = new UsuarioModel();
   constructor() { }
 
   ngOnInit() {
   }
 
+  login(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    console.log(this.usuario);
+  }
 }
