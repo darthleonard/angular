dc53a167a511bfcb3ef45474edf8b25951634f59
darthleonard <leolinuxmx@gmail.com>
2019-10-23 20:00:03 -0500

    Firebase y servicios REST

---
+++ 07-loginApp/src/app/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioModel } from '../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private url = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+  private apikey = 'AIzaSyBE-4U6zSwF9V3gCc9SKZiu45HG_irXYGo';
+
+    // crear nuevo usuario
+    // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+    // login
+    // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+  constructor(private http: HttpClient) { }
+
+  logout() { 
+
+  }
+
+  login(usuario: UsuarioModel) {
+
+  }
+
+  nuevoUsuario(usuario: UsuarioModel) {
+
+  }
+}
