3fa462601a8403f2b874437612960486b21b09b9
Leonardo Gonzalez <leonardogonzalez@mtech-systems.com>
2020-12-17 14:27:09 -0600

    255 InfiniteScroll de peliculas

--- 11-peliculasApp/src/app/pages/home/home.component.ts
+++ 11-peliculasApp/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response';
 import { PeliculasService } from 'src/app/services/peliculas.service';
 
@@ -9,13 +9,27 @@ import { PeliculasService } from 'src/app/services/peliculas.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop) + 1300;
+    const max = (document.documentElement.scrollHeight || document.body.scrollHeight);
+
+    if(pos > max) {
+      this.peliculasService.getCartelera().subscribe(r => {
+        this.movies.push(...r.results);
+      });
+    }
+  }
+
   public movies: Movie[] = [];
+  public moviesSlideShow: Movie[] = [];
 
   constructor(private peliculasService: PeliculasService) {
     this.peliculasService.getCartelera()
     .subscribe( r => {
       console.log(r);
       this.movies = r.results;
+      this.moviesSlideShow = r.results;
     });
   }
 
