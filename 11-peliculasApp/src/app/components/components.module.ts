ccbd99ec739b774548f7f42df844269c5b3946b6
Leonardo Gonzalez <leonardogonzalez@mtech-systems.com>
2020-12-17 17:53:53 -0600

    257 poster pipe

--- 11-peliculasApp/src/app/components/components.module.ts
+++ 11-peliculasApp/src/app/components/components.module.ts
@@ -6,13 +6,15 @@ import { RatingModule } from 'ng-starrating';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
+import { PipesModule } from '../pipes/pipes.module';
 
 @NgModule({
   declarations: [NavbarComponent, SlideshowComponent, PeliculasPosterGridComponent],
   imports: [
     CommonModule,
     RouterModule,
-    RatingModule
+    RatingModule,
+    PipesModule
   ],
   exports: [
     NavbarComponent,
