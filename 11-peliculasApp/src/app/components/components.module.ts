c07a0e44bbf3a065e013d718b448ef891d954c29
Leonardo Gonzalez <leonardogonzalez@mtech-systems.com>
2020-12-17 13:58:56 -0600

    254 Calificacion con estrellas

--- 11-peliculasApp/src/app/components/components.module.ts
+++ 11-peliculasApp/src/app/components/components.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { RatingModule } from 'ng-starrating';
+
+import { NavbarComponent } from './navbar/navbar.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
-
-
 
 @NgModule({
   declarations: [NavbarComponent, SlideshowComponent, PeliculasPosterGridComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    RatingModule
   ],
   exports: [
     NavbarComponent,
