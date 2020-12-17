0055ceb853c1d490e07d2c4ca89002dcb6aee6b2
Leonardo Gonzalez <leonardogonzalez@mtech-systems.com>
2020-12-17 13:13:40 -0600

    250 Swiper - slideshow de peliculas

--- 11-peliculasApp/src/app/components/slideshow/slideshow.component.ts
+++ 11-peliculasApp/src/app/components/slideshow/slideshow.component.ts
@@ -1,15 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response';
+import { Swiper } from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
   @Input() movies: Movie[];
   
   constructor() { }
+  
+  ngAfterViewInit(): void {
+    const mySwiper = new Swiper('.swiper-container', {
+      loop: true
+    });
+  }
 
   ngOnInit(): void {
     console.log(this.movies);
