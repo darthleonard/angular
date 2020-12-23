cdaed7fa81feb5c3838271024f8907441d6049d8
darthleonard <leolinuxmx@gmail.com>
2020-12-23 10:45:41 -0600

    292 Servicio para obtener videos de YouTube

--- 13-youtubeapp/src/app/pages/home/home.component.ts
+++ 13-youtubeapp/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from 'src/app/services/youtube.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private youtuveService: YoutubeService) { }
 
   ngOnInit(): void {
+    this.youtuveService.getVideos().subscribe(v => console.log(v));
   }
 
 }
--- 13-youtubeapp/src/app/services/youtube.service.ts
+++ 13-youtubeapp/src/app/services/youtube.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'AIzaSyAT9dLEOP2bbHLaWcx-nbeF51_8DnzzJkw';
+  private playList = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    
+  }
+
+  getVideos() {
+    const url = `${ this.youtubeUrl }/playlistItems`;
+    const params = new HttpParams()
+      .set('part', 'snippet')
+      .set('maxResults', '10')
+      .set('playlistId', this.playList)
+      .set('key', this.apiKey);
+    return this.http.get(url, { params });
+  }
 }
