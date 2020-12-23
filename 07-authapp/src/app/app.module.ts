6fc5d179883ca22ae5258c92137eb4fd3e0368a6
darthleonard <leolinuxmx@gmail.com>
2020-12-23 17:16:40 -0600

    auth0 implemented

--- 07-authapp/src/app/app.module.ts
+++ 07-authapp/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Import the module from the auth0 SDK
+import { AuthModule } from '@auth0/auth0-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
 import { PreciosComponent } from './components/precios/precios.component';
+import { AuthButtonComponent } from './components/auth-button-component/auth-button-component.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +18,16 @@ import { PreciosComponent } from './components/precios/precios.component';
     NavbarComponent,
     HomeComponent,
     ProtegidaComponent,
-    PreciosComponent
+    PreciosComponent,
+    AuthButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'darthleonard.auth0.com',
+      clientId: 'wPG0XKZ6tIOAuSmc0BnxLCpjeSb7pQyY'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
