3a0a66c546dce795e65555761bf3290745671c0e
darthleonard <leolinuxmx@gmail.com>
2020-12-23 11:36:18 -0600

    295 Mostrar un modal con el video

--- 13-youtubeapp/src/app/pages/home/home.component.ts
+++ 13-youtubeapp/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Video } from 'src/app/models/youtube.models';
 import { YoutubeService } from 'src/app/services/youtube.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-home',
@@ -19,4 +20,24 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  mostrarVideo(video: Video) {
+    console.log(video);
+    Swal.fire({
+      html: `
+      <h4>${ video.title }</h4>
+      <hr>
+      <iframe 
+        width="100%" 
+        src="https://www.youtube.com/embed/${ video.resourceId.videoId }" 
+        frameborder="0" 
+        allow="accelerometer; 
+          autoplay; 
+          clipboard-write; 
+          encrypted-media; 
+          gyroscope; 
+          picture-in-picture" 
+        allowfullscreen>
+      </iframe>`
+    });
+  }
 }
