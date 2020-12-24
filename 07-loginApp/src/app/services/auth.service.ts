04b911bb88bdecd52056145b9e90898b7e109e66
darthleonard <leolinuxmx@gmail.com>
2020-12-23 21:21:59 -0600

    leerToken added to service constructor

--- 07-loginApp/src/app/services/auth.service.ts
+++ 07-loginApp/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UsuarioModel } from '../models/usuario.model';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,16 @@ import { UsuarioModel } from '../models/usuario.model';
 export class AuthService {
   private url = 'https://identitytoolkit.googleapis.com/v1/accounts:';
   private apikey = 'AIzaSyBE-4U6zSwF9V3gCc9SKZiu45HG_irXYGo';
+  private userToken: string;
 
     // crear nuevo usuario
     // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
 
     // login
     // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.leerToken();
+  }
 
   logout() { 
 
@@ -25,10 +29,14 @@ export class AuthService {
       ...usuario,
       returnSecureToken: true
     };
-    console.log(authData);
     return this.http.post(
       `${ this.url }signInWithPassword?key=${ this.apikey }`,
       authData
+    ).pipe(
+      map(r => {
+        this.guardarToken(r['idToken']);
+        return r;
+      })
     );
   }
 
@@ -40,6 +48,25 @@ export class AuthService {
       return this.http.post(
         `${ this.url }signUp?key=${ this.apikey }`,
         authData
+      ).pipe(
+        map(r => {
+          this.guardarToken(r['idToken']);
+          return r;
+        })
       );
+  }
+
+  private guardarToken(idToken: string) {
+    this.userToken = idToken;
+    localStorage.setItem('token', idToken);
+  }
+
+  leerToken() {
+    if(localStorage.getItem('token')) {
+      this.userToken = localStorage.getItem('token');
+    } else {
+      this.userToken = '';
+    }
+    return this.userToken;
   }
 }
