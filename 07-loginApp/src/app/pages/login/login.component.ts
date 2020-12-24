cfef86016cac493d1e16082d773e591ae540bb0d
darthleonard <leolinuxmx@gmail.com>
2020-12-23 21:54:33 -0600

    176 SweetAlert2 Mostrar notificaciones al usuario

--- 07-loginApp/src/app/pages/login/login.component.ts
+++ 07-loginApp/src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { UsuarioModel } from '../../models/usuario.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,8 @@ import { UsuarioModel } from '../../models/usuario.model';
 export class LoginComponent implements OnInit {
   usuario: UsuarioModel = new UsuarioModel();
   
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -20,11 +22,24 @@ export class LoginComponent implements OnInit {
     if (form.invalid) {
       return;
     }
+    // Swal.fire({
+    //   allowOutsideClick: false,
+    //   icon: 'info',
+    //   text: 'espere'
+    // });
+    // Swal.showLoading();
     this.auth.login(this.usuario)
       .subscribe(r => {
         console.log(r);
+        //switchAll.close();
+        this.router.navigateByUrl('/home');
       }, err => {
         console.log(err.error.error.message);
+        // Swal.fire({
+        //   icon: 'error',
+        //   title: 'Error al autenticar'
+        //   text: err.error.error.message
+        // });
       });
   }
 }
--- 07-loginApp/src/app/pages/registro/registro.component.ts
+++ 07-loginApp/src/app/pages/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from 'src/app/models/usuario.model';
 import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -11,7 +12,8 @@ import { AuthService } from 'src/app/services/auth.service';
 export class RegistroComponent implements OnInit {
   usuario: UsuarioModel;
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+    private router: Router) { }
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
@@ -24,6 +26,7 @@ export class RegistroComponent implements OnInit {
     this.auth.nuevoUsuario(this.usuario)
       .subscribe(r => {
         console.log(r);
+        this.router.navigateByUrl('/home');
       }, err => {
         console.log(err.error.error.message);
       });
