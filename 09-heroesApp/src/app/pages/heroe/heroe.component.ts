64b190821e003983b5686d39d73988d68304d80c
darthleonard <leolinuxmx@gmail.com>
2020-12-29 14:21:36 -0600

    220 HTTP PUT Actualizar registro

--- 09-heroesApp/src/app/pages/heroe/heroe.component.ts
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -22,10 +22,17 @@ export class HeroeComponent implements OnInit {
       console.log("formulario no valido");
       return;
     }
-    this.heroesService.crearHeroe(this.heroe)
-      .subscribe(resp => {
-        console.log(resp);
-      });
+    if(this.heroe.id) {
+      this.heroesService.actualizarHeroe(this.heroe)
+        .subscribe(resp => {
+          console.log(resp);
+        });
+    } else {
+      this.heroesService.crearHeroe(this.heroe)
+        .subscribe(resp => {
+          console.log(resp);
+        });
+    }
   }
 
 }
--- 09-heroesApp/src/app/services/heroes.service.ts
+++ 09-heroesApp/src/app/services/heroes.service.ts
@@ -20,4 +20,12 @@ export class HeroesService {
         })
       );
   }
+
+  actualizarHeroe(heroe: HeroeModel) {
+    const heroeTemp = {
+      ...heroe
+    };
+    delete heroeTemp.id;
+    return this.http.put(`${ this.url }/heroes/${ heroe.id }.json`, heroeTemp);
+  }
 }
