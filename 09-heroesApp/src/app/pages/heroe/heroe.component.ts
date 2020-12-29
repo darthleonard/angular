fb1be64743b4f7628ebc9aaeab943c5f9b16a957
darthleonard <leolinuxmx@gmail.com>
2020-12-29 13:49:29 -0600

    217 modelo para manejar eroes y formulario de edicion

--- 09-heroesApp/src/app/pages/heroe/heroe.component.ts
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -1,4 +1,7 @@
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HeroeModel } from 'src/app/models/heroe.model';
 
 @Component({
   selector: 'app-heroe',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroe.component.css']
 })
 export class HeroeComponent implements OnInit {
+  heroe = new HeroeModel();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar(form: NgForm) {
+    if(form.invalid) {
+      console.log("formulario no valido");
+      return;
+    }
+    console.log(form);
+    console.log(this.heroe);
   }
 
 }
