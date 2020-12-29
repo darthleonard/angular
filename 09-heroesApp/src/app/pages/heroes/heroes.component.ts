1c4e9a99cb774891ec5d75734eea64481b70c7b6
darthleonard <leolinuxmx@gmail.com>
2020-12-29 15:12:37 -0600

    224 HTTP DELETE para eliminar registros

--- 09-heroesApp/src/app/pages/heroes/heroes.component.ts
+++ 09-heroesApp/src/app/pages/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroeModel } from 'src/app/models/heroe.model';
 import { HeroesService } from 'src/app/services/heroes.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroes',
@@ -14,6 +15,22 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
     this.heroesService.getHeroes()
       .subscribe(resp => this.heroes = resp);
+  }
+
+  borrarHeroe(heroe: HeroeModel, i: number) {
+    Swal.fire({
+      title: "Estas seguro?",
+      text: `Eliminar a ${ heroe.nombre }`,
+      icon: 'question',
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then(resp => {
+      if(resp.value) {
+        this.heroes.splice(i, 1);
+        this.heroesService.borrarHeroe(heroe.id)
+          .subscribe();
+      }
+    });
   }
 
 }
