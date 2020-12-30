71530f5c197ae7838d15f4f374404adbc0919559
darthleonard <leolinuxmx@gmail.com>
2020-12-30 15:22:29 -0600

    320 Validaciones del archivo desde la directiva

--- 15-fotosfirebase/src/app/directives/ng-drop-files.directive.ts
+++ 15-fotosfirebase/src/app/directives/ng-drop-files.directive.ts
@@ -1,9 +1,11 @@
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output, Host } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
+  @Input() archivos: FileItem[] = [];
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
   constructor() { }
@@ -16,5 +18,34 @@ export class NgDropFilesDirective {
   @HostListener('dragleave', ['$event'])
   public onDragLeave(event: any) {
     this.mouseSobre.emit(false);
+  }
+
+  // validaciones
+  private archivoPuedeSerargado(archivo: File): boolean {
+    if(!this.archivoDropeado(archivo.name) && this.esImagen(archivo.type)) {
+      return true;
+    }
+    return false;
+  }
+
+  private prevenirDetener(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private archivoDropeado(nombre: string): boolean {
+    for(const archivo of this.archivos) {
+      if(archivo.nombreArchivo == nombre) {
+        console.log("el archivo " + nombre + " ya se agrego");
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private esImagen(tipo: string): boolean {
+    return (tipo === '' || tipo === undefined) 
+      ? false 
+      : tipo.startsWith('image');
   }
 }
