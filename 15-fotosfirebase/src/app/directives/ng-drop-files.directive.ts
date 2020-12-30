526546f3b423250aa40c743b6c9f5e4cc5f8790e
darthleonard <leolinuxmx@gmail.com>
2020-12-30 16:42:02 -0600

    321 Obtener archivos y validarlos

--- 15-fotosfirebase/src/app/directives/ng-drop-files.directive.ts
+++ 15-fotosfirebase/src/app/directives/ng-drop-files.directive.ts
@@ -13,11 +13,39 @@ export class NgDropFilesDirective {
   @HostListener('dragover', ['$event'])
   public onDragEnter(event: any) {
     this.mouseSobre.emit(true);
+    this.prevenirDetener(event);
   }
 
   @HostListener('dragleave', ['$event'])
   public onDragLeave(event: any) {
     this.mouseSobre.emit(false);
+  }
+
+  @HostListener('drop', ['$event'])
+  public onDrop(event: any) {
+    const transferencia = this.getTransferencia(event);
+    if(!transferencia) {
+      return;
+    }
+    this.extraerArchivos(transferencia.files);
+    this.prevenirDetener(event);
+    this.mouseSobre.emit(false);
+  }
+
+  private getTransferencia(event: any) {
+    return event.dataTransfer 
+      ? event.dataTransfer 
+      : event.originalEvent.dataTransfer;
+  }
+
+  private extraerArchivos(archivosLista: FileList) {
+    for(const prop in Object.getOwnPropertyNames(archivosLista)) {
+      const archivoTemp = archivosLista[prop];
+      if(this.archivoPuedeSerargado(archivoTemp)) {
+        const nuevo = new FileItem(archivoTemp);
+        this.archivos.push(nuevo);
+      }
+    }
   }
 
   // validaciones
